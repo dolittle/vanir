4234dfcabaa5b4495f0fda0a89843586a587cab6
Einar Ingebrigtsen <einar@dolittle.com>
2020-12-28 09:56:51 +0100

    Preparing for adding portal MS

--- Source/typescript/create-dolittle-app/plopfile.ts
+++ Source/typescript/create-dolittle-app/plopfile.ts
@@ -2,12 +2,18 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import path from 'path';
-import { NodePlopAPI, ActionType } from 'plop';
+import { NodePlopAPI, ActionType, ActionConfig } from 'plop';
 import rootPath from './packageRoot';
 import { Answers } from 'inquirer';
 import { Guid } from '@dolittle/rudiments';
 
 const templatePath = path.join(rootPath, 'templates', 'application.json');
+
+async function addPortalMicroservice(answers: Answers, config?: ActionConfig, plopFileApi?: NodePlopAPI): Promise<string> {
+    console.log('Adding portal');
+    return '';
+}
+
 
 export default function (plop: NodePlopAPI) {
     plop.setGenerator('application', {
@@ -22,12 +28,19 @@ export default function (plop: NodePlopAPI) {
             message: 'Do you want a default UI microservice?'
         }],
         actions: (answers?: Answers) => {
+            const actions: ActionType[] = [];
             answers!.id = Guid.create().toString();
-            return [{
+            actions.push({
                 type: 'add',
                 path: path.join(process.cwd(), 'application.json'),
                 templateFile: templatePath
-            }];
+            });
+
+            if (answers?.portal) {
+                actions.push(addPortalMicroservice);
+            }
+
+            return actions;
         }
     });
 };
