1520b0615db713d0017ae393896b9050118e2539
Einar Ingebrigtsen <einar@dolittle.com>
2020-12-29 11:26:00 +0100

    Removing copyright headers

--- Source/typescript/create-dolittle-microservice/templates/web/Source/{{ pascalCase name }}/Web/webpack.config.js
+++ Source/typescript/create-dolittle-microservice/templates/web/Source/{{ pascalCase name }}/Web/webpack.config.js
@@ -1,9 +1,6 @@
-// Copyright (c) Dolittle. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
 const webpack = require('@dolittle/vanir-webpack/frontend');
 module.exports = (env, argv) => {
-    return webpack(env, argv, '/_/{{lowerCase name}}', config => {
+    return webpack(env, argv, '{{lowerCase uiPath}}', config => {
         config.devServer.port = 9003;
     });
 };
