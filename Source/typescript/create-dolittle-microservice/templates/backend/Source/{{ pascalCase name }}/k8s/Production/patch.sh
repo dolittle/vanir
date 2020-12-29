0fac19ee73250eb36510d1af5da3705268da6274
Einar Ingebrigtsen <einar@dolittle.com>
2020-12-29 11:14:52 +0100

    Fixing wrong casing for using helper lowerCase <- pun not intended

--- Source/typescript/create-dolittle-microservice/templates/backend/Source/{{ pascalCase name }}/k8s/Production/patch.sh
+++ Source/typescript/create-dolittle-microservice/templates/backend/Source/{{ pascalCase name }}/k8s/Production/patch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kubectl patch --namespace application-{{applicationId}} deployment {{lowerCase applicationName}}-prod-{{lowercase name}}
+kubectl patch --namespace application-{{applicationId}} deployment {{lowerCase applicationName}}-prod-{{lowerCase name}}
