8101235ed9bbc0a659bfd78d8e474a660d45c675
Einar Ingebrigtsen <einar@dolittle.com>
2021-02-19 06:33:42 +0100

    Fixing proxy path for graphql

--- Samples/Source/Portal/Web/webpack.config.js
+++ Samples/Source/Portal/Web/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = (env, argv) => {
     return webpack(env, argv, '/', config => {
         config.devServer.port = process.env.port || 9000;
         config.devServer.proxy = {
-            '//graphql': 'http://localhost:3000',
+            '/graphql': 'http://localhost:3000',
             '/api': 'http://localhost:3000'
         };
     }, 'SampleApp');
