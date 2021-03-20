a5453b86eb170752b2df8728228499bcc0fbbd11
Einar Ingebrigtsen <einar@dolittle.com>
2021-03-20 18:31:28 +0100

    More cache test

--- Source/DotNET/Backend/Vanir.cs
+++ Source/DotNET/Backend/Vanir.cs
@@ -4,7 +4,7 @@
 namespace Dolittle.Vanir.Backend
 {
     /// <summary>
-    /// Something something something something
+    /// Something something something something something
     /// </summary>
     public class Vanir
     {
