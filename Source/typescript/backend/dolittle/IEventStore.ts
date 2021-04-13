b40ec6d71f419f4693baae3477566496f99d0f5b
Einar Ingebrigtsen <einar@dolittle.com>
2021-04-13 20:45:59 +0200

    Adding missing methods from the IEventStore interface

--- Source/typescript/backend/dolittle/IEventStore.ts
+++ Source/typescript/backend/dolittle/IEventStore.ts
@@ -5,10 +5,45 @@ import { Guid } from '@dolittle/rudiments';
 import { EventType } from '@dolittle/sdk.artifacts';
 import { Cancellation } from '@dolittle/sdk.resilience';
 
-import { IEventStore as IActualEventStore, UncommittedEvent, CommitEventsResult } from '@dolittle/sdk.events';
+import {
+    IEventStore as IActualEventStore,
+    UncommittedEvent,
+    CommitEventsResult,
+    UncommittedAggregateEvents,
+    CommitAggregateEventsResult,
+    CommitForAggregateBuilder,
+    CommittedAggregateEvents,
+    AggregateRootId,
+    AggregateRootVersion,
+    EventTypeId,
+    EventSourceId,
+
+} from '@dolittle/sdk.events';
 
 export abstract class IEventStore implements IActualEventStore {
-    abstract commit(event: any, eventSourceId: Guid | string, eventType?: EventType | Guid | string, cancellation?: Cancellation): Promise<CommitEventsResult>;
+    /** @inheritdoc */
+    abstract commit(event: any, eventSourceId: EventSourceId | Guid | string, eventType?: EventType | EventTypeId | Guid | string, cancellation?: Cancellation): Promise<CommitEventsResult>;
+
+    /** @inheritdoc */
     abstract commit(eventOrEvents: UncommittedEvent | UncommittedEvent[], cancellation?: Cancellation): Promise<CommitEventsResult>;
-    abstract commitPublic(event: any, eventSourceId: Guid | string, eventType?: EventType | Guid | string, cancellation?: Cancellation): Promise<CommitEventsResult>;
+
+    /** @inheritdoc */
+    abstract commitPublic(event: any, eventSourceId: EventSourceId |Guid | string, eventType?: EventType | Guid | string, cancellation?: Cancellation): Promise<CommitEventsResult>;
+
+    /** @inheritdoc */
+    abstract commitForAggregate(event: any, eventSourceId: EventSourceId | Guid | string, aggregateRootId: AggregateRootId, expectedAggregateRootVersion: AggregateRootVersion, eventType?: EventType | EventTypeId | Guid | string, cancellation?: Cancellation): Promise<CommitAggregateEventsResult>;
+
+    /** @inheritdoc */
+    abstract commitForAggregate(events: UncommittedAggregateEvents, cancellation?: Cancellation): Promise<CommitAggregateEventsResult>;
+
+    /** @inheritdoc */
+    abstract forAggregate(aggregateRootId: AggregateRootId): CommitForAggregateBuilder;
+
+    /** @inheritdoc */
+    abstract fetchForAggregate(aggregateRootId: AggregateRootId, eventSourceId: EventSourceId, cancellation?: Cancellation): Promise<CommittedAggregateEvents>;
+
+    /** @inheritdoc */
+    abstract fetchForAggregateSync(aggregateRootId: AggregateRootId, eventSourceId: EventSourceId, cancellation?: Cancellation): CommittedAggregateEvents;
 }
+
+
