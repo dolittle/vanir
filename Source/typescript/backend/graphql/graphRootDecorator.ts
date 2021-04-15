531fd879e9bd58881be899765696fa6caeee498f
Einar Ingebrigtsen <einar@dolittle.com>
2021-04-15 20:47:07 +0200

    Defined the decorator for graphRoot-ing

---
+++ Source/typescript/backend/graphql/graphRootDecorator.ts
@@ -0,0 +1,12 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+/**
+ * Decorator to use for setting route for a containing class of queries or mutations.
+ * @param {string} path Path to use
+ */
+export function graphRoot(path: string) {
+    return function (target: any) {
+
+    };
+}
