3a6fd3f2cbfedbbddd8956c40dd50b5a5af370c0
Einar Ingebrigtsen <einar@dolittle.com>
2021-04-21 15:20:01 +0200

    Changing to FieldConfig

--- Source/typescript/backend/graphql/SchemaRoute.ts
+++ Source/typescript/backend/graphql/SchemaRoute.ts
@@ -1,11 +1,11 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { GraphQLField } from 'graphql';
+import { GraphQLField, GraphQLFieldConfig, GraphQLFieldConfigMap, argsToArgsConfig } from 'graphql/type/definition';
 
 export class SchemaRoute {
     readonly children: SchemaRoute[] = [];
-    readonly items: GraphQLField<any, any>[] = [];
+    readonly items: GraphQLFieldConfig<any, any>[] = [];
 
     constructor(readonly path: string, readonly localName: string, readonly typeName: string) { }
 
@@ -14,6 +14,14 @@ export class SchemaRoute {
     }
 
     addItem(item: GraphQLField<any, any>) {
-        this.items.push(item);
+        this.items.push({
+            description: item.description,
+            type: item.type,
+            args: argsToArgsConfig(item.args),
+            resolve: item.resolve,
+            subscribe: item.subscribe,
+            deprecationReason: item.deprecationReason,
+            extensions: item.extensions
+        });
     }
 }
