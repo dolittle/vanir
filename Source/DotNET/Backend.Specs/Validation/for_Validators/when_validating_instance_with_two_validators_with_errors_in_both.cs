02325535278c7b2c567588a8259021c42a42f03e
Einar Ingebrigtsen <einar@dolittle.com>
2021-05-07 13:31:39 +0200

    Adding specifactions for validation

---
+++ Source/DotNET/Backend.Specs/Validation/for_Validators/when_validating_instance_with_two_validators_with_errors_in_both.cs
@@ -0,0 +1,60 @@
+using Dolittle.Vanir.Backend.Reflection;
+using Dolittle.Vanir.Backend.Validation;
+using FluentValidation;
+using FluentValidation.Results;
+using Machine.Specifications;
+using Moq;
+using It = Machine.Specifications.It;
+
+namespace Dolittle.Vanir.Backend.Specs.Validation.for_Validators
+{
+    public class when_validating_instance_with_two_validators_with_errors_in_both
+    {
+        class TypeWithValidation
+        {
+            public string Something { get; set; } = "";
+        }
+
+        class FirstValidator : AbstractValidator<TypeWithValidation>
+        {
+            public const string ErrorMessage = "FirstValidatorErrorMessage";
+
+            public FirstValidator()
+            {
+                RuleFor(_ => _.Something).Matches("42").WithMessage(ErrorMessage);
+            }
+        }
+
+        class SecondValidator : AbstractValidator<TypeWithValidation>
+        {
+            public const string ErrorMessage = "SecondValidatorErrorMessage";
+            public SecondValidator()
+            {
+                RuleFor(_ => _.Something).Matches("43").WithMessage(ErrorMessage);
+            }
+        }
+
+        static Validators validators;
+        static Mock<ITypes> types;
+        static Mock<IContainer> container;
+
+        static ValidationResult result;
+
+        Establish context = () =>
+        {
+            types = new();
+            container = new();
+
+            types.Setup(_ => _.FindMultiple(typeof(AbstractValidator<>))).Returns(new[] { typeof(FirstValidator), typeof(SecondValidator) });
+            container.Setup(_ => _.Get(typeof(FirstValidator))).Returns(new FirstValidator());
+            container.Setup(_ => _.Get(typeof(SecondValidator))).Returns(new SecondValidator());
+            validators = new Validators(types.Object, container.Object);
+        };
+
+        Because of = () => result = validators.Validate(new TypeWithValidation());
+
+        It should_return_result_with_two_errors = () => result.Errors.Count.ShouldEqual(2);
+        It should_have_first_validators_message = () => result.Errors[0].ErrorMessage.ShouldEqual(FirstValidator.ErrorMessage);
+        It should_have_second_validators_message = () => result.Errors[1].ErrorMessage.ShouldEqual(SecondValidator.ErrorMessage);
+    }
+}
