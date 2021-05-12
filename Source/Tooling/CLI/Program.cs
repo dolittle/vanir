459bf07a6887fc70db866285b0608defc6bc9c60
Einar Ingebrigtsen <einar@dolittle.com>
2021-05-12 15:49:08 +0200

    Sorted usings

--- Source/Tooling/CLI/Program.cs
+++ Source/Tooling/CLI/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.CommandLine;
 using Autofac;
+using System.CommandLine;
 
 namespace Dolittle.Vanir.CLI
 {
