85cfa0657cf7d4fdc2819d6515efd1d46bf674c8
Einar Ingebrigtsen <einar@dolittle.com>
2021-05-16 09:37:12 +0200

    Adding initialization

--- Source/typescript/features/index.ts
+++ Source/typescript/features/index.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+
 export * from './BooleanFeatureToggleStrategy';
 export * from './featureDecorator';
 export * from './Features';
@@ -9,3 +10,14 @@ export * from './FeatureToggles';
 export * from './IFeaturesProvider';
 export * from './IFeatureToggles';
 export * from './IFeatureToggleStrategy';
+import { container } from 'tsyringe';
+import { IFeaturesProvider } from './IFeaturesProvider';
+import { FeaturesProvider } from './FeaturesProvider';
+import { constructor } from '@dolittle/vanir-dependency-inversion';
+import { IFeatureToggles } from './IFeatureToggles';
+import { FeatureToggles } from './FeatureToggles';
+
+export function initialize(): void {
+    container.registerSingleton(IFeaturesProvider as constructor<IFeaturesProvider>, FeaturesProvider);
+    container.registerSingleton(IFeatureToggles as constructor<IFeatureToggles>, FeatureToggles);
+}
