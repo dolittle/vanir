5fbd478bd34e8cab37606508345e6a37b099aef2
Einar Ingebrigtsen <einar@dolittle.com>
2021-05-16 08:54:51 +0200

    Making Features immutable

--- Source/typescript/features/Features.ts
+++ Source/typescript/features/Features.ts
@@ -6,5 +6,54 @@ import { IFeatureToggleStrategy } from './IFeatureToggleStrategy';
 /**
  * Represents a set of {@link Features}.
  */
-export class Features extends Map<string, IFeatureToggleStrategy> {
+export class Features implements ReadonlyMap<string, IFeatureToggleStrategy> {
+    private readonly _features: Map<string, IFeatureToggleStrategy>;
+
+    /**
+     * Initializes a new instance of {@link Features}.
+     * @param {Map<string, IFeatureToggleStrategy>} [features] Optional map of features.
+     */
+    constructor(features?: Map<string, IFeatureToggleStrategy>) {
+        this._features = features || new Map<string, IFeatureToggleStrategy>();
+    }
+
+    /** @inheritdoc */
+    forEach(callbackfn: (value: IFeatureToggleStrategy, key: string, map: ReadonlyMap<string, IFeatureToggleStrategy>) => void, thisArg?: any): void {
+        this._features.forEach(callbackfn);
+    }
+
+    /** @inheritdoc */
+    get(key: string): IFeatureToggleStrategy | undefined {
+        return this._features.get(key);
+    }
+
+    /** @inheritdoc */
+    has(key: string): boolean {
+        return this._features.has(key);
+    }
+
+    /** @inheritdoc */
+    get size(): number {
+        return this._features.size;
+    }
+
+    /** @inheritdoc */
+    [Symbol.iterator](): IterableIterator<[string, IFeatureToggleStrategy]> {
+        return this._features[Symbol.iterator]();
+    }
+
+    /** @inheritdoc */
+    entries(): IterableIterator<[string, IFeatureToggleStrategy]> {
+        return this._features.entries();
+    }
+
+    /** @inheritdoc */
+    keys(): IterableIterator<string> {
+        return this._features.keys();
+    }
+
+    /** @inheritdoc */
+    values(): IterableIterator<IFeatureToggleStrategy> {
+        return this._features.values();
+    }
 }
