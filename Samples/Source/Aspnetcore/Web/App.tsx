c99a33eed8fb38847688531c95ab5b30e75d0fb3
Einar Ingebrigtsen <einar@dolittle.com>
2021-05-18 20:43:02 +0200

    Putting in feature test

--- Samples/Source/Aspnetcore/Web/App.tsx
+++ Samples/Source/Aspnetcore/Web/App.tsx
@@ -2,14 +2,18 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import React from 'react';
-import { PrimaryButton } from '@fluentui/react';
-import { withViewModel } from '@dolittle/vanir-react';
+import { Toggle } from '@fluentui/react';
+import { useFeature, withViewModel } from '@dolittle/vanir-react';
 import { AppViewModel } from './AppViewModel';
 
 export const App = withViewModel(AppViewModel, ({ viewModel }) => {
+    const myFirstMethodFeature = useFeature('my.first.method.feature');
+    const myFirstClassFeature = useFeature('my.first.class.feature');
+
     return (
         <>
-            <PrimaryButton>Click me</PrimaryButton>
+            <Toggle label="my.first.method.feature" checked={myFirstMethodFeature} onText="On" offText="Off" />
+            <Toggle label="my.first.class.feature" checked={myFirstClassFeature} onText="On" offText="Off" />
         </>
     );
 });
