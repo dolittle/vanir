29195fbad110a2cc5865027387f6d5cc676e6b02
Einar Ingebrigtsen <einar@dolittle.com>
2021-05-19 19:57:27 +0200

    Fixing JSDoc comment

--- Source/typescript/features/IFeatureToggle.ts
+++ Source/typescript/features/IFeatureToggle.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 /**
- * Defines a strategy for toggling a {@link Feature}
+ * Defines a toggle for a feature.
  */
 export interface IFeatureToggle {
     /**
