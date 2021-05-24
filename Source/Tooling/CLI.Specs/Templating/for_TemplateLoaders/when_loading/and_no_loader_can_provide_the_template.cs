498e6fcd47e435a393b75040ebe054281d94bfa2
Einar Ingebrigtsen <einar@dolittle.com>
2021-05-24 07:28:27 +0200

    Fixing namespace after move

---
+++ Source/Tooling/CLI.Specs/Templating/for_TemplateLoaders/when_loading/and_no_loader_can_provide_the_template.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Machine.Specifications;
+using It = Machine.Specifications.It;
+
+namespace Dolittle.Vanir.CLI.Templating.for_TemplateLoaders.when_loading
+{
+    public class and_no_loader_can_provide_the_template : given.two_template_loaders
+    {
+        const string template = "my.template.hbs";
+
+        static Exception result;
+
+        Establish context = () =>
+        {
+            first_loader.Setup(_ => _.Exists(template)).Returns(false);
+            second_loader.Setup(_ => _.Exists(template)).Returns(false);
+        };
+
+        Because of = () => result = Catch.Exception(() => loaders.Load(template));
+
+        It should_throw_template_not_found = () => result.ShouldBeOfExactType<TemplateNotFound>();
+    }
+}
