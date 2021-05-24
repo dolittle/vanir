bb3f8220301d48b26e8b5e780842b0ac6a52d4ad
Einar Ingebrigtsen <einar@dolittle.com>
2021-05-24 07:29:16 +0200

    Cleanup

--- Source/DotNET/Backend.Specs/Validation/for_Validators/when_validating_instance_with_two_validators_with_errors_in_both.cs
+++ Source/DotNET/Backend.Specs/Validation/for_Validators/when_validating_instance_with_two_validators_with_errors_in_both.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Dolittle.Vanir.Backend.Reflection;
-using Dolittle.Vanir.Backend.Validation;
 using FluentValidation;
 using FluentValidation.Results;
 using Machine.Specifications;
