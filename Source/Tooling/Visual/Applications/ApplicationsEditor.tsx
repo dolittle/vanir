a3b439d4924a002b6b7643aea59b634f89f4d2c5
Einar Ingebrigtsen <einar@dolittle.com>
2021-05-26 18:42:12 +0200

    Recreated basic node editor

--- Source/Tooling/Visual/Applications/ApplicationsEditor.tsx
+++ Source/Tooling/Visual/Applications/ApplicationsEditor.tsx
@@ -1,14 +1,45 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { PrimaryButton } from '@fluentui/react';
 import React from 'react';
+import ReactFlow, { ArrowHeadType, Controls } from 'react-flow-renderer';
+import { MicroserviceNode } from './MicroserviceNode';
+
+const elements = [
+    { id: '1', data: { label: 'Flattened JSON Producer' }, position: { x: 250, y: 5 } },
+    // you can also pass a React component as a label
+    { id: '2', data: { label: <div>eCommerce</div> }, position: { x: 100, y: 100 } },
+    { id: '3', data: { label: <div>Product Structure Extractor</div> }, position: { x: 200, y: 100 } },
+    { id: '4', data: { label: <div>Portal</div> }, position: { x: 100, y: 200 } },
+    { id: 'e1-2', source: '2', target: '1', animated: true, arrowHeadType: ArrowHeadType.ArrowClosed },
+    { id: 'e3-2', source: '3', target: '2', animated: true, arrowHeadType: ArrowHeadType.ArrowClosed },
+    {
+        id: '5',
+        type: 'microservice',
+        position: { x: 300, y: 100 },
+        data: { text: 'Something' },
+    },
+
+    {
+        id: '6',
+        type: 'microservice',
+        position: { x: 400, y: 100 },
+        data: { text: 'Another node' },
+    },
+    { id: 'e2-2', source: '5', target: '6', sourceHandle: 'a', targetHandle: 'a', animated: true },
+];
+
+const nodeTypes = {
+    microservice: MicroserviceNode,
+};
+
 
 export const ApplicationsEditor = () => {
     return (
         <>
-            Hello from application
-            <PrimaryButton>Hello there</PrimaryButton>
+            <ReactFlow elements={elements} nodeTypes={nodeTypes}>
+                <Controls />
+            </ReactFlow>
         </>
     );
 };
---
+++ Source/Tooling/Visual/Applications/MicroserviceNode.tsx
@@ -0,0 +1,73 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import React, { FC, useState } from 'react';
+import {  Handle,  Node, Position } from 'react-flow-renderer';
+import { DocumentCard, DocumentCardActions, DocumentCardTitle, DocumentCardType, IButtonProps } from '@fluentui/react';
+
+const onActionClick = (): void => {
+    //vscode.postMessage({ type: 'hello' });
+
+    //ev.stopPropagation();
+    //ev.preventDefault();
+};
+
+
+const documentCardActions: IButtonProps[] = [
+    {
+        iconProps: { iconName: 'Play' },
+        ariaLabel: 'Start',
+        title: 'Start',
+    },
+    {
+        iconProps: { iconName: 'AllApps' },
+        onClick: onActionClick,
+        ariaLabel: 'Features',
+        title: 'Features',
+    },
+    {
+        iconProps: { iconName: 'BullseyeTarget' },
+        ariaLabel: 'Event Horizon',
+        title: 'Event Horizon',
+    },
+    {
+        iconProps: { iconName: 'SearchData' },
+        ariaLabel: 'Event Store',
+        title: 'Event Store',
+    },
+];
+
+const customNodeStyles = {
+    background: '#9CA8B3',
+    color: '#FFF',
+    padding: 0,
+};
+
+
+export const MicroserviceNode: FC<Node> = ({ data }) => {
+
+    return (
+        <div style={customNodeStyles}>
+            <Handle id="a" type="source" position={Position.Top} style={{ borderRadius: 0 }} />
+
+            <DocumentCard type={DocumentCardType.normal}>
+                <DocumentCardTitle title={data.text} />
+                <DocumentCardActions actions={documentCardActions} />
+            </DocumentCard>
+            <Handle
+                type="source"
+                position={Position.Left}
+                id="b"
+                style={{ borderRadius: 0 }}
+            />
+            <Handle
+                type="target"
+                position={Position.Right}
+                id="c"
+                style={{ borderRadius: 0 }}
+            />
+            <Handle id="d" type="source" position={Position.Bottom} style={{ borderRadius: 0 }} />
+        </div>
+    );
+
+};
