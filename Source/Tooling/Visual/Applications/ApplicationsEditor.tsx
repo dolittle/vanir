ee2c2ef36c27c6c9543eb0785adef535629554ac
Einar Ingebrigtsen <einar@dolittle.com>
2021-05-26 19:16:32 +0200

    Testing out connection hookup

--- Source/Tooling/Visual/Applications/ApplicationsEditor.tsx
+++ Source/Tooling/Visual/Applications/ApplicationsEditor.tsx
@@ -1,32 +1,42 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import React from 'react';
-import ReactFlow, { ArrowHeadType, Controls } from 'react-flow-renderer';
+import React, { useState } from 'react';
+import ReactFlow, { ArrowHeadType, Connection, ConnectionMode, Controls, Edge } from 'react-flow-renderer';
 import { MicroserviceNode } from './MicroserviceNode';
 
-const elements = [
-    { id: '1', data: { label: 'Flattened JSON Producer' }, position: { x: 250, y: 5 } },
-    // you can also pass a React component as a label
-    { id: '2', data: { label: <div>eCommerce</div> }, position: { x: 100, y: 100 } },
-    { id: '3', data: { label: <div>Product Structure Extractor</div> }, position: { x: 200, y: 100 } },
-    { id: '4', data: { label: <div>Portal</div> }, position: { x: 100, y: 200 } },
-    { id: 'e1-2', source: '2', target: '1', animated: true, arrowHeadType: ArrowHeadType.ArrowClosed },
-    { id: 'e3-2', source: '3', target: '2', animated: true, arrowHeadType: ArrowHeadType.ArrowClosed },
+const initialElements = [
     {
-        id: '5',
+        id: '1',
         type: 'microservice',
-        position: { x: 300, y: 100 },
-        data: { text: 'Something' },
+        position: { x: 0, y: 0 },
+        data: { text: 'Flattened JSON Producer' },
+    },
+    {
+        id: '2',
+        type: 'microservice',
+        position: { x: 50, y: 200 },
+        data: { text: 'eCommerce' },
     },
 
     {
-        id: '6',
+        id: '3',
         type: 'microservice',
-        position: { x: 400, y: 100 },
-        data: { text: 'Another node' },
+        position: { x: 600, y: 400 },
+        data: { text: 'Product Structure Extractor' },
     },
-    { id: 'e2-2', source: '5', target: '6', sourceHandle: 'a', targetHandle: 'a', animated: true },
+
+    {
+        id: '4',
+        type: 'microservice',
+        position: { x: 300, y: 500 },
+        data: { text: 'Portal' },
+    }
+
+    /*,
+    { id: 'e1-2', source: '2', target: '1', animated: true, arrowHeadType: ArrowHeadType.ArrowClosed },
+    { id: 'e3-2', source: '3', target: '2', animated: true, arrowHeadType: ArrowHeadType.ArrowClosed },
+    { id: 'e2-2', source: '5', target: '6', sourceHandle: 'a', targetHandle: 'a', animated: true },*/
 ];
 
 const nodeTypes = {
@@ -35,9 +45,25 @@ const nodeTypes = {
 
 
 export const ApplicationsEditor = () => {
+    const [elements, setElements] = useState(initialElements);
+
+    const onConnect = (connection: Edge | Connection) => {
+        const newConnection = {
+            id: `${connection.source}${connection.sourceHandle}-${connection.target}${connection.targetHandle}`,
+            animated: true,
+            arrowHeadType: ArrowHeadType.ArrowClosed
+        };
+
+        setElements([...elements, {...newConnection, ... connection} as any]);
+    };
+
     return (
         <>
-            <ReactFlow elements={elements} nodeTypes={nodeTypes}>
+            <ReactFlow
+                onConnect={onConnect}
+                connectionMode={ConnectionMode.Strict}
+                elements={elements}
+                nodeTypes={nodeTypes}>
                 <Controls />
             </ReactFlow>
         </>
