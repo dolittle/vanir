2f970bcb049c64a625b8d43bd26fd0094fc0a0c8
Einar Ingebrigtsen <einar@dolittle.com>
2021-06-07 14:11:16 +0200

    Adding Xml docs

--- Source/DotNET/Backend/Container.cs
+++ Source/DotNET/Backend/Container.cs
@@ -5,15 +5,20 @@ using System;
 
 namespace Dolittle.Vanir.Backend
 {
-    public class Container : Backend.IContainer
+    /// <summary>
+    /// Represents an implementation of <see cref="IContainer"/>.
+    /// </summary>
+    public class Container : IContainer
     {
-        internal static IServiceProvider    ServiceProvider;
+        internal static IServiceProvider ServiceProvider;
 
+        /// <inheritdoc/>
         public T Get<T>()
         {
             return (T)ServiceProvider.GetService(typeof(T));
         }
 
+        /// <inheritdoc/>
         public object Get(Type type)
         {
             return ServiceProvider.GetService(type);
--- Source/DotNET/Backend/IContainer.cs
+++ Source/DotNET/Backend/IContainer.cs
@@ -5,10 +5,23 @@ using System;
 
 namespace Dolittle.Vanir.Backend
 {
+    /// <summary>
+    /// Defines a container to be used internally by Vanir.
+    /// </summary>
     public interface IContainer
     {
+        /// <summary>
+        /// Get the instance of a type.
+        /// </summary>
+        /// <typeparam name="T">Type to get instance of</typeparam>
+        /// <returns>The instance.</returns>
         T Get<T>();
 
+        /// <summary>
+        /// Get the instance of a type.
+        /// </summary>
+        /// <param name="type">Type to get instance of.</param>
+        /// <returns>The instance.</returns>
         object Get(Type type);
     }
 }
